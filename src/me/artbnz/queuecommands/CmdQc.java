7880372f421be13a51b239ee41c6f5a3c5b3a3c8
artbnzz <artzakung2010@hotmail.com>
2019-04-10 02:45:41 +0700

    Fix bugs on handle player online/offline

--- src/me/artbnz/queuecommands/CmdQc.java
+++ src/me/artbnz/queuecommands/CmdQc.java
@@ -25,30 +25,44 @@ public class CmdQc implements CommandExecutor {
 					sender.sendMessage(ChatColor.RED + "[QueueCommands] : You do not have permission to do this!");
 					return false;
 				}
+				if (args.length < 3) {
+					sender.sendMessage(ChatColor.RED + "/qc addp [player] [command] <args>" + ChatColor.AQUA
+							+ " Send command by player as soon as possible.");
+					return false;
+				}
 				Player target = sender.getServer().getPlayer(args[1]);
-				if (target == null) {
-					String oldArgs = args[1];
-					args[1] = "";
-					String command = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
-							.replace("addp  ", "").replace("&", "§");
-					args[1] = oldArgs;
-					PlayerFile yaml = Queue.getOfflinePlayerYaml(args[1]);
+				PlayerFile yaml = Queue.getOfflinePlayerYaml(args[1]);
+				if(target == null) {
 					if (!yaml.contains("commands")) {
+						String oldArgs = args[1];
+						args[1] = "";
+						String command = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
+								.replace("addp  ", "").replace("&", "§");
+						args[1] = oldArgs;
 						List<String> commands = new ArrayList<String>();
 						commands.add(command);
 						yaml.createNewStringList("commands", commands);
 						yaml.save();
+						sender.sendMessage(ChatColor.RED + "[QueueCommands] : " + args[1] + " is now offline.");
 						sender.sendMessage(
-								ChatColor.GOLD + "Added " + args[1] + " to the queue with the command /" + command);
+								ChatColor.RED + "Added " + args[1] + " to the queue with the command /" + command);
 						return false;
 					}
-					List<String> commands = yaml.getStringList("commands");
-					commands.add(command);
-					yaml.set("commands", commands);
-					yaml.save();
-					sender.sendMessage(
-							ChatColor.GOLD + "Added " + args[1] + " to the queue with the command /" + command);
-					return false;
+					if (yaml.contains("commands")) {
+						String oldArgs = args[1];
+						args[1] = "";
+						String command = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
+								.replace("addp  ", "").replace("&", "§");
+						args[1] = oldArgs;
+						List<String> commands = yaml.getStringList("commands");
+						commands.add(command);
+						yaml.set("commands", commands);
+						yaml.save();
+						sender.sendMessage(ChatColor.RED + "[QueueCommands] : " + args[1] + " is now offline.");
+						sender.sendMessage(
+								ChatColor.RED + "Added " + args[1] + " to the queue with the command /" + command);
+						return false;
+					}
 				}
 				if (target.isOnline()) {
 					String oldArgs = args[1];
@@ -66,6 +80,10 @@ public class CmdQc implements CommandExecutor {
 					sender.sendMessage(ChatColor.RED + "[QueueCommands] : You do not have permission to do this!");
 					return false;
 				}
+				if (args.length != 2) {
+					sender.sendMessage(ChatColor.RED + "/qc clear [player]" + ChatColor.AQUA + " Clear queue player");
+					return false;
+				}
 				PlayerFile yaml = Queue.getOfflinePlayerYaml(args[1]);
 				yaml.delete();
 				sender.sendMessage(ChatColor.GOLD + args[1] + "s queue cleared!");
@@ -75,31 +93,57 @@ public class CmdQc implements CommandExecutor {
 					sender.sendMessage(ChatColor.RED + "[QueueCommands] : You do not have permission to do this!");
 					return false;
 				}
+				if (args.length < 3) {
+					sender.sendMessage(ChatColor.RED + "/qc message [player] [message]" + ChatColor.AQUA
+							+ " Send message to player as soon as possible.");
+					return false;
+				}
 				PlayerFile yaml = Queue.getOfflinePlayerYaml(args[1]);
-				if (yaml.contains("messages")) {
+				Player target = sender.getServer().getPlayer(args[1]);
+				if(target == null) {
+					if (yaml.contains("messages")) {
+						String oldArgs = args[1];
+						args[1] = "";
+						String message = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
+								.replace("message  ", "").replace("&", "§");
+						args[1] = oldArgs;
+						List<String> messages = yaml.getStringList("messages");
+						messages.add(message);
+						yaml.set("messages", messages);
+						yaml.save();
+						sender.sendMessage(ChatColor.RED + "[QueueCommands] : " + args[1] + " is now offline.");
+						sender.sendMessage(ChatColor.RED + "Added message to " + args[1] + "s queue!");
+						return false;
+					}
+					if (!yaml.contains("messages")) {
+						String oldArgs = args[1];
+						args[1] = "";
+						String message = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
+								.replace("message  ", "").replace("&", "�");
+						args[1] = oldArgs;
+						List<String> messages = new ArrayList<String>();
+						messages.add(message);
+						yaml.createNewStringList("messages", messages);
+						yaml.save();
+						sender.sendMessage(ChatColor.RED + "[QueueCommands] : " + args[1] + " is now offline.");
+						sender.sendMessage(ChatColor.RED + "Added message to " + args[1] + "s queue!");
+						return false;
+					}
+				}
+				if (target.isOnline()) {
 					String oldArgs = args[1];
 					args[1] = "";
 					String message = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
 							.replace("message  ", "").replace("&", "§");
 					args[1] = oldArgs;
-					List<String> messages = yaml.getStringList("messages");
-					messages.add(message);
-					yaml.set("messages", messages);
-					yaml.save();
-					sender.sendMessage(ChatColor.GOLD + "Added message to " + args[1] + "s queue!");
-					return false;
-				}
-				if (!yaml.contains("messages")) {
-					String oldArgs = args[1];
-					args[1] = "";
-					String message = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
-							.replace("message  ", "").replace("&", "�");
-					args[1] = oldArgs;
 					List<String> messages = new ArrayList<String>();
 					messages.add(message);
-					yaml.createNewStringList("messages", messages);
-					yaml.save();
-					sender.sendMessage(ChatColor.GOLD + "Added message to " + args[1] + "s queue!");
+					while ((messages.size() != 0) && (messages.get(0) != null)) {
+						target.sendMessage((String) messages.get(0));
+						messages.remove(messages.get(0));
+					}
+					sender.sendMessage(ChatColor.GREEN + "[QueueCommands] : " + target
+							+ " is now online, Successful run message.");
 					return false;
 				}
 			}
@@ -108,22 +152,14 @@ public class CmdQc implements CommandExecutor {
 					sender.sendMessage(ChatColor.RED + "[QueueCommands] : You do not have permission to do this!");
 					return false;
 				}
+				if (args.length < 3) {
+					sender.sendMessage(ChatColor.RED + "/qc addc [player] [command] <args>" + ChatColor.AQUA
+							+ " Send command by console to player as soon as possible.");
+					return false;
+				}
 				PlayerFile yaml = Queue.getOfflinePlayerYaml(args[1]);
 				Player target = sender.getServer().getPlayer(args[1]);
-				if (target.isOnline()) {
-					String oldArgs = args[1];
-					args[1] = "";
-					String consoleCmd = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
-							.replace("addc  ", "").replace("&", "§");
-					args[1] = oldArgs;
-					List<String> commands = new ArrayList<String>();
-					commands.add(consoleCmd);
-					while ((commands.size() != 0) && (commands.get(0) != null)) {
-						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), (String) commands.get(0));
-						commands.remove(commands.get(0));
-					}
-					sender.sendMessage(ChatColor.GREEN+"[QueueCommands] : "+target+" is now online, Successful run command.");
-				} else {
+				if(target == null) {
 					if (yaml.contains("consoleCommands")) {
 						String oldArgs = args[1];
 						args[1] = "";
@@ -134,9 +170,9 @@ public class CmdQc implements CommandExecutor {
 						commands.add(consoleCmd);
 						yaml.set("consoleCommands", commands);
 						yaml.save();
-						sender.sendMessage(ChatColor.RED+"[QueueCommands] : "+target+" is now offline.");
-						sender.sendMessage(ChatColor.RED + "Added " + args[1]
-								+ " to the queue with the console command /" + consoleCmd);
+						sender.sendMessage(ChatColor.RED + "[QueueCommands] : " + args[1] + " is now offline.");
+						sender.sendMessage(ChatColor.RED + "Added " + args[1] + " to the queue with the console command /"
+								+ consoleCmd);
 						return false;
 					}
 					if (!yaml.contains("consoleCommands")) {
@@ -149,13 +185,28 @@ public class CmdQc implements CommandExecutor {
 						commands.add(consoleCmd);
 						yaml.createNewStringList("consoleCommands", commands);
 						yaml.save();
-						sender.sendMessage(ChatColor.RED+"[QueueCommands] : "+target+" is now offline.");
-						sender.sendMessage(ChatColor.RED + "Added " + args[1]
-								+ " to the queue with the console command /" + consoleCmd);
+						sender.sendMessage(ChatColor.RED + "[QueueCommands] : " + args[1] + " is now offline.");
+						sender.sendMessage(ChatColor.RED + "Added " + args[1] + " to the queue with the console command /"
+								+ consoleCmd);
 						return false;
 					}
 				}
-
+				if (target.isOnline()) {
+					String oldArgs = args[1];
+					args[1] = "";
+					String consoleCmd = Arrays.toString(args).replace("[", "").replaceAll("[],,]", "")
+							.replace("addc  ", "").replace("&", "§");
+					args[1] = oldArgs;
+					List<String> commands = new ArrayList<String>();
+					commands.add(consoleCmd);
+					while ((commands.size() != 0) && (commands.get(0) != null)) {
+						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), (String) commands.get(0));
+						commands.remove(commands.get(0));
+					}
+					sender.sendMessage(ChatColor.GREEN + "[QueueCommands] : " + target
+							+ " is now online, Successful run command.");
+					return false;
+				}
 			}
 			if ((!args[0].equals("addp")) && (!args[0].equals("addc")) && (!args[0].equals("clear"))
 					&& (!args[0].equals("message"))) {
